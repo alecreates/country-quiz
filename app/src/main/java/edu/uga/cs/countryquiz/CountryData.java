a6d453d874698a191731ee388d0a64a218880b59
Alexia Toma <alexia.g.toma.328@gmail.com>
2026-03-30 19:22:40 -0400

    added retrieveAllCountries function in CountryData

--- app/src/main/java/edu/uga/cs/countryquiz/CountryData.java
+++ app/src/main/java/edu/uga/cs/countryquiz/CountryData.java
@@ -2,8 +2,13 @@ package edu.uga.cs.countryquiz;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class CountryData {
@@ -39,14 +44,63 @@ public class CountryData {
         return db.isOpen();
     }
 
-    // Retrieve all job leads and return them as a List.
-    // This is how we restore persistent objects stored as rows in the job leads table in the database.
-    // For each retrieved row, we create a new JobLead (Java POJO object) instance and add it to the list.
 
-    // public List<Country> retrieveAllCountries() {}
+    // Retrieve all countries and return them as a List.
 
+    public List<Country> retrieveAllCountries() {
+        ArrayList<Country> countries = new ArrayList<>();
+        Cursor cursor = null;
+        int columnIndex;
+        try {
+            // Execute the select query and get the Cursor to iterate over the retrieved rows
+            cursor = db.query( CountryQuizDBHelper.TABLE_COUNTRIES, allColumns,
+                    null, null, null, null, null );
 
+            // collect all job leads into a List
+            if( cursor != null && cursor.getCount() > 0 ) {
 
+                while( cursor.moveToNext() ) {
+
+                    if( cursor.getColumnCount() >= 5) {
+
+                        // get all attribute values of this job lead
+                        columnIndex = cursor.getColumnIndex( CountryQuizDBHelper.COUNTRIES_COLUMN_ID );
+                        long id = cursor.getLong( columnIndex );
+                        columnIndex = cursor.getColumnIndex( CountryQuizDBHelper.COUNTRIES_COLUMN_NAME );
+                        String name = cursor.getString( columnIndex );
+                        columnIndex = cursor.getColumnIndex( CountryQuizDBHelper.COUNTRIES_COLUMN_CAPITAL );
+                        String capital = cursor.getString( columnIndex );
+                        columnIndex = cursor.getColumnIndex( CountryQuizDBHelper.COUNTRIES_COLUMN_CONTINENT );
+                        String continent = cursor.getString( columnIndex );
+                        columnIndex = cursor.getColumnIndex( CountryQuizDBHelper.COUNTRIES_COLUMN_CODE );
+                        String code = cursor.getString( columnIndex );
+
+                        // create a new JobLead object and set its state to the retrieved values
+                        Country country = new Country( name, capital, continent, code );
+                        country.setId(id); // set the id (the primary key) of this object
+                        // add it to the list
+                        countries.add( country );
+                        Log.d(DEBUG_TAG, "Retrieved JobLead: " + country);
+                    }
+                }
+            }
+            if( cursor != null )
+                Log.d( DEBUG_TAG, "Number of records from DB: " + cursor.getCount() );
+            else
+                Log.d( DEBUG_TAG, "Number of records from DB: 0" );
+        }
+        catch( Exception e ){
+            Log.d( DEBUG_TAG, "Exception caught: " + e );
+        }
+        finally{
+            // we should close the cursor
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+        // return a list of retrieved job leads
+        return countries;
+    }
 
     // Store a new country in the DB
     public Country storeCountry ( Country country ) {
